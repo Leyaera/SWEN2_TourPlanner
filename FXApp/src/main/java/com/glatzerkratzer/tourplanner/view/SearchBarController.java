d984814e60cbf78502a11059d66271c6f0597d68
Carmen Kratzer <if20b111@technikum-wien.at>
2022-04-20 18:35:37 +0200

    SearchBar integrated in TourOverview instead of MainWindow and search on KeyEvent (KeyTyped)

--- FXApp/src/main/java/com/glatzerkratzer/tourplanner/view/SearchBarController.java
+++ FXApp/src/main/java/com/glatzerkratzer/tourplanner/view/SearchBarController.java
@@ -5,6 +5,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.input.KeyEvent;
 
 import java.util.Locale;
 
@@ -31,7 +32,11 @@ public class SearchBarController {
         searchTextField.textProperty().bindBidirectional( searchBarViewModel.searchStringProperty() );
     }
 
-    public void onSearchButton(ActionEvent actionEvent) {
+    /*public void onSearchButton(ActionEvent actionEvent) {
         searchBarViewModel.doSearch();
     }
+
+     */
+
+    public void onKeyTypedSearch(KeyEvent keyEvent) { searchBarViewModel.doSearch(); }
 }
